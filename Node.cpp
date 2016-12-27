ab532d1ef98ad00a4185d481b1562ccfcbeff7e6
fcifuen1 <fedcu96@gmail.com>
2016-12-27 01:19:02 -0500

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,19 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <class T>
+class Node
+{
+  public:
+  
+    Node();
+    Node(T input);
+    ~Node();
+  
+    T data;
+    Node<T>* next;
+};
+
+#include "Node.cpp"
+
+#endif
