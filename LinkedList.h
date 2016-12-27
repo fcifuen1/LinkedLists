58395a158831d10e4c7e1255f4c7f4789f3daa65
fcifuen1 <fedcu96@gmail.com>
2016-12-27 01:31:45 -0500

    Create LinkedList.h

---
+++ LinkedList.h
@@ -0,0 +1,34 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Node.h" // <iostream>
+
+template <class T>
+class LinkedList
+{
+  public:
+    LinkedList();
+    ~LinkedList();
+    
+    // Returns the number of nodes in the list
+    int size() const;
+    
+    // Returns whether a list has data or not
+    bool empty() const;
+  
+    // Inserts a node at the head
+    void insert();
+    
+    // Inserts a node into the nth position 
+    void insertNth(int n);
+  
+    // Erases a list object
+    void clear();
+  
+  private:
+    int size;
+    Node<T>* head;
+    Node<T>* tail;
+};
+
+#endif
